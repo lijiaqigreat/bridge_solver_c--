12e482d60a9904eb356f93cffea72d1036036b76
Jiaqi Li <lijiaqigreat@gmail.com>
2014-01-29 12:06:09 -0600

    decrypt file using rc4test.c, add two sample file

--- rc4test.c
+++ rc4test.c
@@ -2,56 +2,51 @@
 #include <stdio.h>
 
 typedef struct TRC4State_t {
-	int x, y;
 	unsigned char buf[256];
+	int x, y;
 } TRC4State;
 
 void setup_rc4(TRC4State *state, char *key, int keylen)
 {
-    unsigned tmp;
-    int x, y;
+    unsigned int tmp,x,y;
 
 	// use only first 256 characters of key 
     if (keylen > 256) 
-		keylen = 256;
+	keylen = 256;
 
     // make RC4 perm and shuffle 
     for (x = 0; x < 256; x++)
         state->buf[x] = x;
-
+    state->x=state->y=0;
+ 
     for (x = y = 0; x < 256; x++) {
         y = (y + state->buf[x] + key[x % keylen]) & 255;
         tmp = state->buf[x]; 
-		state->buf[x] = state->buf[y]; 
-		state->buf[y] = tmp;
+	state->buf[x] = state->buf[y]; 
+	state->buf[y] = tmp;
     }
     state->x = x;
     state->y = y;
-
-	// DEBUG: Temporary while Steve fixes bug.
-	state->y = 0;
 }
 
 unsigned long endecrypt_rc4(unsigned char *buf, unsigned long len, TRC4State *state)
 {
-   int x, y; 
-   unsigned char *s, tmp;
-   unsigned long n;
-
-   n = len;
-   x = state->x;
-   y = state->y;
-   s = state->buf;
-   while (len--) {
-      x = (x + 1) & 255;
-      y = (y + s[x]) & 255;
-      tmp = s[x]; s[x] = s[y]; s[y] = tmp;
-      tmp = (s[x] + s[y]) & 255;
-      *buf++ ^= s[tmp];
-   }
-   state->x = x;
-   state->y = y;
-   return n;
+    int x=state->x;
+    int y=state->y;
+    int i;
+    unsigned char *s=(unsigned char*)state;
+    unsigned char tmp;
+    for(i=0;i<len;i++){
+        x=(x+1)&255;
+        y=(y+s[x])&255;
+        tmp=s[x];
+        s[x]=s[y];
+        s[y]=tmp;
+        buf[i]^=s[(s[x]+s[y])&255];
+    }
+    state->x=x;
+    state->y=y;
+    return 0;
 }
 
 int main(void)
@@ -64,7 +59,7 @@ int main(void)
 
 	/* setup RC4 for use */
 	setup_rc4(&state, key, strlen(key));
-	f = fopen("Eg/2014/MyDesign.bdc", "rb");
+	f = fopen("Eg/2014/test2.bdc", "rb");
 	if (!f) {
 		fprintf(stderr, "can't open input file\n");
 		return 1;
@@ -74,10 +69,7 @@ int main(void)
 	buf[n] = '\0';
 
 	/* encrypt buffer */
-	if (endecrypt_rc4(buf, n, &state) != n) {
-		printf("endecrypt error\n");
-		return -1;
-	}
+	endecrypt_rc4(buf, n, &state);
 	printf("decrypted text:%s\n", buf);
 	return 0;
 }
